3a7e5bb5d1873055a3051f1e1cf203ee510334ed
Chrillaz <totalchriz@gmail.com>
2020-10-20 13:23:19 +0200

    release v1.1.3

--- structured-data.php
+++ structured-data.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Structured Data
  * Description: Inserts structured data to page head
- * Version: 1.1.1
+ * Version: 1.1.3
  * Author: Noor Digital Agency
  * Author URI: https://noordigital.com
  */
