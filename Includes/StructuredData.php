78c4845d2e2db2d053e6c07a05bf766097cb35f1
Chrillaz <totalchriz@gmail.com>
2021-03-18 15:12:16 +0100

    downgraded role cap to edit_post

--- Includes/StructuredData.php
+++ Includes/StructuredData.php
@@ -79,7 +79,7 @@ class StructuredData {
     add_menu_page( 
       $this->plugin_name, 
       $this->plugin_name, 
-      'read', 
+      'edit_posts', 
       $this->plugin_prefix, 
       array( $this, $this->plugin_prefix . '_options_page' ),
       'dashicons-index-card'
